ea8625680be055c84705c9e04ed60c6fe9fe26dd
DESKTOP-8VK1MG7\USER <jesmqz@gmail.com>
2021-05-31 01:31:27 -0400

    Fixs

--- api.js
+++ api.js
@@ -5,29 +5,29 @@ const { reverseText } = require('./lib/general')
 // const api = express.Router()
 const api = asyncify(express.Router())
 
-api.use(function(err, req, res, next) {
-    console.error(err.stack)
+api.use(function (err, req, res, next) {
+  console.error(err.stack)
 
-    res.status(500).send({ error: '500 ERROR something happened' })
+  res.status(500).send({ error: '500 ERROR something happened' })
 })
 
-api.get('/iecho', async(req, res, next) => {
-    const { text } = req.query
-    let responseJson = {}
+api.get('/iecho', async (req, res, next) => {
+  const { text } = req.query
+  const responseJson = {}
 
-    if (text === undefined) {
-        res.status(400).send({ error: 'no text' })
-    } else {
-        let reversedText = reverseText(text)
-        responseJson.text = reversedText
-            // if text is palindrome then add a flag
-        text === reversedText ? responseJson.palindrome = true : responseJson.palindrome = false
-        res.status(200).json(responseJson)
-    }
+  if (text === undefined) {
+    res.status(400).send({ error: 'no text' })
+  } else {
+    const reversedText = reverseText(text)
+    responseJson.text = reversedText
+    // if text is palindrome then add a flag
+    text === reversedText ? responseJson.palindrome = true : responseJson.palindrome = false
+    res.status(200).json(responseJson)
+  }
 })
 
-api.get('*', function(req, res, next) {
-    res.status(404).send({ error: '404 NOT FOUND' })
-});
+api.get('*', function (req, res, next) {
+  res.status(404).send({ error: '404 NOT FOUND' })
+})
 
 module.exports = api
--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const asyncify = require('express-asyncify')
 const api = require('./api')
 const cors = require('cors')
 
-
 // const app = express()
 const app = asyncify(express())
 const server = http.createServer(app)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 const host = 'localhost'
 
@@ -17,18 +16,17 @@ app.use(cors())
 app.use('/', api)
 
 const handleFatalError = (err) => {
-    console.error(err.message)
-    console.error(err.stack)
-    process.exit(1)
+  console.error(err.message)
+  console.error(err.stack)
+  process.exit(1)
 }
 
 if (!module.parent) {
-    process.on('uncaughtException', handleFatalError)
-    process.on('unhandledRejection', handleFatalError)
+  process.on('uncaughtException', handleFatalError)
+  process.on('unhandledRejection', handleFatalError)
 
-    server.listen(PORT, () => {
-
-        console.log(`API is running at ${ host } on port ${ PORT }`)
-    })
+  server.listen(PORT, () => {
+    console.log(`API is running at ${host} on port ${PORT}`)
+  })
 }
 module.exports = app
